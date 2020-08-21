cd757d1c938f0dd9cde9a19bb1496469ca72314b
Jose Paulo Gabriel Dos Santos <40630290+JotaPeGabriel@users.noreply.github.com>
2020-08-21 20:28:36 -0300

    Adesão de "tempo"

--- excel_OpenPyXL.py
+++ excel_OpenPyXL.py
@@ -1,13 +1,15 @@
 from openpyxl import load_workbook
 from random import choice
 """Importar a base do Excel e Randomizar a escolha das celulas"""
-wb = load_workbook('coisas.xlsx')
-plan = wb['Planilha1']
+wb = load_workbook('database_test.xlsx')
+plan = wb['dados']
 lista = plan['A']
+plant = wb['tempo']
+listat = plant['A']
 print(len(lista))
+print(len(listat))
 
-for click in range(300):
-    arroba = choice(lista)
-    print(f'{click + 1} - Voce Marcou: {arroba.value}')
-
-input("DIGITE ALGO PARA SAIR")
+for click in range(len(lista)):
+    dado = choice(lista)
+    tempo = choice(listat)
+    print(f'A palavra foi {dado.value} e o tempo {tempo.value} segundos')
